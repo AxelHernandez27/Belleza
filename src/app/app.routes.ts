aebbc9a6e487e0bb53d21d6b9e50056f261645a9
Jorge Axel Hernandez <20001544@alumnos.utleon.edu.mx>
2024-11-15 22:08:06 -0600

    servicios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'servicios', component: ServiciosComponent},
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
