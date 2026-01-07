20e65ca55c168e072170adae37eb55d3a54e79ed
Brenda Ruiz <ruizhernandezbrendac@gmail.com>
2026-01-06 22:55:55 -0600

    pagina de privacidad para api

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { ContactoComponent } from './contacto/contacto.component';
 import { PortafolioComponent } from './portafolio/portafolio.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { PrivacidadComponent } from './privacidad/privacidad.component';
 
 export const routes: Routes = [
     { path: 'inicio', component: InicioComponent },
@@ -11,6 +12,7 @@ export const routes: Routes = [
     { path: 'contacto', component: ContactoComponent },
     { path: 'portafolio', component: PortafolioComponent },
     { path: 'portafolio/:id', component: PortafolioComponent },
+    { path: 'privacidad', component: PrivacidadComponent },
     { path: '', redirectTo: '/inicio', pathMatch: 'full' },
     { path: '*', redirectTo: '/inicio', pathMatch: 'full' }
 ];
